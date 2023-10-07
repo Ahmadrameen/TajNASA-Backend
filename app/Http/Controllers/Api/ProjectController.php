734164c2f34fafed021e2ed0c14419469b275b4f
Ahmad Rameen <a.alizai@rebus.tj>
2023-10-07 19:06:01 +0500

    Added pagination to projects

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('photos')->withCount('members')->get();
+        $projects = Project::with('photos')->withCount('members')->paginate('10');
 
         return response()->json([
             'status' => true,
